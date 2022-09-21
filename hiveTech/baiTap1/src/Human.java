78db74a4ac45cbbf2391e205ce7d58ba5a3d3d2b
TranLinh3020 <114060806+TranLinh3020@users.noreply.github.com>
2022-09-21 15:17:24 +0700

    Add files via upload

---
+++ hiveTech/baiTap1/src/Human.java
@@ -0,0 +1,11 @@
+public class Human {
+    String walk;
+    String eat;
+    
+    public void walk(){
+        System.out.println("I'm walking.");
+    }
+    public void eat(){
+        System.out.println("I'm eatting.");
+    }
+}
